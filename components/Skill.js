8f0cc3c600919446c6f028cd4e7a0f0bf19a75a5
Luksuz <lukamindjek@gmail.com>
2023-12-16 10:48:53 +0100

    refactored the skills section and added the certs

--- components/Skill.js
+++ components/Skill.js
@@ -1,68 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button } from "react-bootstrap";
 
-export default function Skill({ skillName, skillGrade, imgURL, size }) {
-  function rateSkill(grade) {
-    if (grade === 1) {
-      return (
-        <>
-          <Button variant="danger"> </Button>
-          <Button variant="secondary"></Button>
-          <Button variant="secondary"></Button>
-          <Button variant="secondary"></Button>
-          <Button variant="secondary"></Button>
-        </>
-      );
-    } else if (grade === 2) {
-      return (
-        <>
-          <Button variant="warning"></Button>
-          <Button variant="warning"></Button>
-          <Button variant="secondary"></Button>
-          <Button variant="secondary"></Button>
-          <Button variant="secondary"></Button>
-        </>
-      );
-    } else if (grade === 3) {
-      return (
-        <>
-          <Button variant="warning"></Button>
-          <Button variant="warning"></Button>
-          <Button variant="warning"></Button>
-          <Button variant="secondary"></Button>
-          <Button variant="secondary"></Button>
-        </>
-      );
-    } else if (grade === 4) {
-      return (
-        <>
-          <Button variant="success"></Button>
-          <Button variant="success"></Button>
-          <Button variant="success"></Button>
-          <Button variant="success"></Button>
-          <Button variant="secondary"></Button>
-        </>
-      );
-    } else if (grade === 5) {
-      return (
-        <>
-          <Button variant="success"></Button>
-          <Button variant="success"></Button>
-          <Button variant="success"></Button>
-          <Button variant="success"></Button>
-          <Button variant="success"></Button>
-        </>
-      );
-    }
-  }
+export default function Skill({ skillName, imgURL, subSkills }) {
 
-  const healthBar = rateSkill(skillGrade);
+  const mappedSubSkills = subSkills.map((subSkill) => {
+    return <li>{subSkill}</li>;
+  });
 
   return (
-        <div className="col-11 col-md-5 m-3 border border-black rounded-4 p-3 shadow-lg">
-          <img src={imgURL} width={size} alt="Skill Icon"/>
-          <h1>{skillName}</h1>
-            {healthBar}
-        </div>
+    <div className="col-11 col-md-5 m-3 border border-black rounded-4 p-3 shadow-lg">
+    <div className="d-flex gap-2 justify-content-center aling-items-center">
+      <img src={imgURL} width={"13%"}/>
+      <h2>{skillName}</h2>
+    </div>
+    <br></br>
+    <ul>
+      {mappedSubSkills}
+    </ul>
+  </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -90,23 +90,19 @@ function Home() {
                 >
                   <div className="col-12 p-3">
                     <p>
-                      <span className="display-6">H</span>ello! I'm Luka, a
-                      self-taught full-stack developer with a passion for coding
-                      and problem-solving. I recently completed a Python
-                      developer qualification course at Algebra University,
-                      where I honed my skills and knowledge in programming. One
-                      of my proudest achievements is the creation of an
-                      <a
-                        style={{ textDecoration: "none", color: "#4477CE" }}
-                        href="http://ai-stories-frontend.s3-website-us-east-1.amazonaws.com/"
-                        target="_blank"
-                      >
-                        {" "}
-                        AI stories generator app
+                      <span className="fs-2">H</span>ello, i'm Luka, a seasoned
+                      full-stack developer with a year of hands-on experience in
+                      software engineering and cloud technologies. What sets me
+                      apart is my unique journey - I didn't pursue a college
+                      education, yet my competence in the field speaks volumes.
+                      Through my unmatched indefatigability and perspicacity,
+                      i've learned my way around tech. Together with a Python
+                      developer diploma and certifications in Google Cloud and
+                      AWS, coupled with 16 projects on my{" "}
+                      <a href="https://Github.com/Luksuz" target="_blank">
+                        Github profile
                       </a>
-                      , which includes captivating images to bring stories to
-                      life. Alongside this major project, I've worked on various
-                      mini projects that showcase my versatility and creativity.
+                      , i have tangible proof to back it up.
                     </p>
                   </div>
                 </div>
@@ -117,7 +113,7 @@ function Home() {
                   transition={{ delay: 2, duration: 1.5 }}
                 >
                   <hr></hr>
-                  <div className="col-8 ">
+                  <div className="col-8 mb-3">
                     <img
                       className="border border-3 shadow-lg"
                       src="/Luka.webp"
@@ -125,84 +121,66 @@ function Home() {
                       width="100%"
                     />
                   </div>
+                  <div>
+                    <h1>Certifications</h1>
+                    <hr className="w-100"></hr>
+                  </div>
+                  <div className="d-flex flex-column flex-sm-row text-center justify-content-around align-items-center">
+                    <div>
+                      <img src="/skillsIcons/GCP.png" width={"20%"} />
+                      <div className="fs-3">
+                        <a
+                          className="link"
+                          target="_blank"
+                          href="https://google.accredible.com/87878445-c838-4fb4-bd17-53afe1d9332f"
+                        >
+                          Associate Cloud Engineer
+                        </a>{" "}
+                        <img src="/blank.png" width={"6%"} />
+                      </div>
+                      <p className="text-secondary">Google Cloud certified</p>
+                    </div>
+                    <div>
+                      <img src="/skillsIcons/python.png" width={"30%"} />
+                      <div className="fs-3">
+                        <a
+                          className="link"
+                          target="_blank"
+                          href="https://blog-client-prod.s3.us-east-1.amazonaws.com/Luka%20Min%C4%91ek.pdf?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEBAaCXVzLWVhc3QtMSJGMEQCIFwL4BiZ%2BICImTwUFCgoaIxFQCqv6Q34f1uI2I85lTEBAiBZ%2B77QS39qZ%2F6GGr7DdgU3qGTdCVgG4suqbyjGzPJZXirtAgiJ%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F8BEAAaDDg3OTUwMDE4ODgyNCIMSAHenbJ1e1JLMVAPKsECzp235K6LOprklB0fUQgldzmyvGkTyd5NOPHkwREDDdn0Xga%2FaPuvt1Qg7BuXkDea7ugIagbxgNK7M2tO07wtZZ1Lku5szcuPLwqxbjJgrURZ7coZoXgee1RAFTwb6J8Cuq6Kpu187QGokLqD79r5frhSrclqOjaYrkwl5oEs7gytemprDWQT%2FiYlylrPme78%2B2j98G6tWPVEwFlO759xn5r%2F4kCT3bWEA5uPMyqMbP%2BB54ciXZqVTsL8UiBn3LTkQyft8s%2F%2BxWGtnLBRG0F%2F7EnxL%2BtMthb%2FS%2BUZRjusSERlPdnwbmLmtMz2lgbXhADTakQhszd9I3%2BZ7Zc1yg1sJscXRNf4JidoIPhIQh8qo%2Bod5EkQUF%2FVHkg%2BvIFm73B%2FbQ8OzyzS24anoZYBn31x%2BQQuvtUOAmpVLFSw10rn6or2MLim9asGOrQCrdAJpEMetl%2FLYqMO6xie3OK0umQSkOzHx26uBvD%2FWQVDwF2eg0wuRFGm35Jzz9CWYc%2F5fjNkqHATKk%2Bu8A%2FfHGoxRxDxKNmrvs%2BgCOWkX6pqgJZC%2FyHVi1CGCg%2BoQCoom9xUwsVKBgAUA3C5PCzvyh7fYFKJ909OGaZhzNSilv4LnbcMDIdieKywOJDDFDAkyRb6kRvaObvOdgvzsTzgaGz4JSURkDkNK%2BWM%2FmrBkkE4uJqN6bitF9rXYvUfKbL3EljONSmcy513cC9Hl1nXx7nUy%2FDHEdNn5DBOg0Dui8lmjeelujPPZ0AOeMC0o6V13S7y6BKcK63oJOFt7hY2qH8ZdjNip7xs3LrisqPmpz1LVP3dAq7sWy6rbc9mzJNHj01c8Z6ywFz0RQP16FZPhL%2FcI%2Fs%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20231216T090600Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIA4ZRSL3SMHP4NS6GN%2F20231216%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=c2fedfc07fd3d7113d4b0ec7aefba4dd808af152064cf21a77bde81eddd5aa51"
+                        >
+                          Python developer
+                        </a>{" "}
+                        <img src="/blank.png" width={"10%"} />
+                      </div>
+                      <p className="text-secondary">Algebra lab</p>
+                    </div>
+                    <div>
+                      <img src="/skillsIcons/AWS.png" width={"25%"} />
+                      <div className="fs-3">
+                        <a
+                          className="link"
+                          target="_blank"
+                          href="https://www.credly.com/badges/3adc51ac-3a0c-46d0-961a-83a596cb5693/public_url"
+                        >
+                          Associate Cloud Developer
+                        </a>{" "}
+                        <img src="/blank.png" width={"7%"} />
+                      </div>
+                      <p className="text-secondary">AWS certified</p>
+                    </div>
+                  </div>
                 </div>
               </div>
               <div className="container d-flex justify-content-center align-items-center">
                 <div className="row my-5 justify-content-center align-items-center">
                   <h1>Skills</h1>
                   <hr />
-                  <Skill
-                    skillName="Javascript"
-                    skillGrade={4}
-                    imgURL="/skillsIcons/js.png"
-                    size="40px"
-                  />
-                  <Skill
-                    skillName="HTML"
-                    skillGrade={5}
-                    imgURL="/skillsIcons/html.png"
-                    size="40px"
-                  />
-                  <Skill
-                    skillName="CSS"
-                    skillGrade={4}
-                    imgURL="/skillsIcons/css-3.png"
-                    size="40px"
-                  />
-                  <Skill
-                    skillName="React"
-                    skillGrade={4}
-                    imgURL="/skillsIcons/react.png"
-                    size="40px"
-                  />
-                  <Skill
-                    skillName="Node.js"
-                    skillGrade={3}
-                    imgURL="/skillsIcons/nodejs.png"
-                    size="40px"
-                  />
-                  <Skill
-                    skillName="Next.js"
-                    skillGrade={3}
-                    imgURL="/skillsIcons/nextjs.webp"
-                    size="50px"
-                  />
-                  <Skill
-                    skillName="Bootstrap"
-                    skillGrade={5}
-                    imgURL="/skillsIcons/bootstrap.png"
-                    size="40px"
-                  />
-                  <Skill
-                    skillName="Tailwind"
-                    skillGrade={2}
-                    imgURL="/skillsIcons/tailwind.png"
-                    size="40px"
-                  />
-                  <Skill
-                    skillName="Python"
-                    skillGrade={5}
-                    imgURL="/skillsIcons/python.png"
-                    size="40px"
-                  />
-                  <Skill
-                    skillName="flask"
-                    skillGrade={4}
-                    imgURL="/skillsIcons/flask.png"
-                    size="40px"
-                  />
-                  <Skill
-                    skillName="MongoDB"
-                    skillGrade={3}
-                    imgURL="/skillsIcons/mongodb.png"
-                    size="40px"
-                  />
-                  <Skill
-                    skillName="SQLite"
-                    skillGrade={3}
-                    imgURL="/skillsIcons/sqlite.png"
-                    size="50px"
-                  />
+                  <Skill skillName={"Python"} subSkills={["Django", "Flask", "Selenium", "BeautifulSoup4", "OpenAPI"]} imgURL={"/skillsIcons/python.png"}/>
+                  <Skill skillName={"JavaScript"} subSkills={["ReactJS", "NextJS", "NodeJS", "ExpressJS"]} imgURL={"/skillsIcons/js.png"}/>
+                  <Skill skillName={"HTML & CSS"} subSkills={["HTML5", "SCSS", "Bootstrap", "Tailwind*"]} imgURL={"/skillsIcons/html&css.png"}/>
+                  <Skill skillName={"Databases"} subSkills={["MongoDB", "MySQL", "SQLite"]} imgURL={"/skillsIcons/sql.png"}/>
+                  <Skill skillName={"Cloud"} subSkills={["Amazon web services", "Google cloud"]} imgURL={"/skillsIcons/cloud.png"}/>
+                  <Skill skillName={"Other"} subSkills={["Git", "Github", "Postman", "Netlify", "Vercel", "Docker", "Kubernetes", "Linux*"]} imgURL={"/skillsIcons/other.png"}/>
                 </div>
               </div>
             </PageTransition>
@@ -338,10 +316,14 @@ function Home() {
                   <div className="col-12">
                     <h1>Projects</h1>
                     <p>
-                      Here are some of the projects I've worked on. See all 16 of my projects on my <a href="https://Github.com/Luksuz" target="_blank">Github</a> profile.
+                      Here are some of the projects I've worked on. See all 16
+                      of my projects on my{" "}
+                      <a href="https://Github.com/Luksuz" target="_blank">
+                        Github
+                      </a>{" "}
+                      profile.
                       <br></br>
-                      Click on the
-                      image to view the project.
+                      Click on the image to view the project.
                     </p>
                   </div>
                 </div>
@@ -349,22 +331,21 @@ function Home() {
                 <hr></hr>
 
                 <div className="row">
-                   <div className="col-12">
+                  <div className="col-12">
                     <h1>Book reviews app</h1>
                     <p>
                       This app is a book review platform that allows users to
                       search for books, read and post book reviews.
                     </p>
-                   </div>
-                  <Link href="http://book-reviews.s3-website-us-east-1.amazonaws.com">
+                  </div>
+                  <Link target="_blank" href="http://book-reviews.s3-website-us-east-1.amazonaws.com">
                     <img
-                    src="./book-reviews.png"
-                    alt="GetActive-ai app image"
-                    width="100%"
-                    className="rounded rounded-5"
+                      src="./book-reviews.png"
+                      alt="GetActive-ai app image"
+                      width="100%"
+                      className="rounded rounded-5"
                     />
-                    
-                    </Link>
+                  </Link>
                 </div>
 
                 <hr></hr>
@@ -380,7 +361,7 @@ function Home() {
                       their conversation with their virtual trainer.
                     </p>
                   </div>
-                  <Link href="http://get-active-frontend.s3-website-us-east-1.amazonaws.com">
+                  <Link target="_blank" href="http://get-active-frontend.s3-website-us-east-1.amazonaws.com">
                     <img
                       src="./getactive-ai.webp"
                       alt="GetActive-ai app image"
@@ -407,7 +388,7 @@ function Home() {
                       experience.
                     </p>
                   </div>
-                  <Link href="http://ai-stories-frontend.s3-website-us-east-1.amazonaws.com/">
+                  <Link target="_blank" href="http://ai-stories-frontend.s3-website-us-east-1.amazonaws.com/">
                     <img
                       src="./ai-stories.png"
                       alt="AI-stories image"
@@ -428,7 +409,14 @@ function Home() {
                       interface and a score counter.
                     </p>
                     <div className="d-flex justify-content-center align-items-center">
-                    <iframe src="https://giphy.com/embed/APeJNsdVKWA0MsmzWV" width="306" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/APeJNsdVKWA0MsmzWV"></a></p>
+                      <iframe
+                        src="https://giphy.com/embed/APeJNsdVKWA0MsmzWV"
+                        width="306"
+                        height="480"
+                        frameBorder="0"
+                        class="giphy-embed"
+                        allowFullScreen
+                      ></iframe>
                     </div>
                   </div>
                 </div>
@@ -442,7 +430,7 @@ function Home() {
                     </p>
                   </div>
 
-                  <Link href="/quiz-app/">
+                  <Link target="_blank" href="/quiz-app/">
                     <img
                       src="./quizz-app.webp"
                       alt="Quizz app image"
@@ -459,7 +447,7 @@ function Home() {
                       This one is for all of my fellow gamers out there! Find
                       the best game deals across 12 different game stores!
                     </p>
-                    <Link href="/game-finder-app">
+                    <Link target="_blank" href="/game-finder-app">
                       <img
                         src="./game-search.webp"
                         alt="game search app image"
@@ -477,7 +465,7 @@ function Home() {
                       You somehow dont have contacts on your phone? Thats where
                       i got you covered! Enjoy your own simple contacts app!
                     </p>
-                    <Link href="https://luxury-contacts.netlify.app">
+                    <Link target="_blank" href="https://luxury-contacts.netlify.app">
                       <img
                         src="./contacts-app.png"
                         alt="contacts app image"
@@ -496,7 +484,7 @@ function Home() {
                       anywhere in the world? Yeah, me neither, but here's a
                       weather app anyway!
                     </p>
-                    <Link href="/weather-app/">
+                    <Link target="_blank" href="/weather-app/">
                       <img
                         src="./weather-app.webp"
                         alt="a weather app image"
@@ -520,7 +508,7 @@ function Home() {
                       "Yeah, I think so too, but I'm not sure, wish there was an
                       app that we all have so we could track it easier".
                     </p>
-                    <Link href="https://shopping-cart-to-buy.netlify.app">
+                    <Link target="_blank" href="https://shopping-cart-to-buy.netlify.app">
                       <img
                         src="./shopping-cart-app.webp"
                         alt="Shopping cart app image"
@@ -550,7 +538,10 @@ function Home() {
                     <p style={{ color: "gray" }} className="mt-5">
                       Click on the image to view the blog
                     </p>
-                    <Link href="http://blog-client-prod.s3-website-us-east-1.amazonaws.com" target="_blank">
+                    <Link
+                      href="http://blog-client-prod.s3-website-us-east-1.amazonaws.com"
+                      target="_blank"
+                    >
                       <img
                         className="shadow-lg"
                         src="/luka-blog.webp"
